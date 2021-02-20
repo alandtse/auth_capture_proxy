aab19c793db097b20ec46e08070a90d33799c33c
Alan D. Tse <alandtse@gmail.com>
2021-02-20 13:38:13 -0800

    style: add typing for stackoverflow

--- authcaptureproxy/stackoverflow.py
+++ authcaptureproxy/stackoverflow.py
@@ -16,11 +16,10 @@ def get_open_port() -> int:
     Returns
         int: a random open port. This does not guarantee the port will remain open and may fail if there is a race condition.
     """
-    # pylint: disable=invalid-name
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 0))
     s.listen(1)
-    port = s.getsockname()[1]
+    port: int = s.getsockname()[1]
     s.close()
     return port
 
